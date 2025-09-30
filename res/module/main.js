4f11e3980ae3c89ef1247871908d7d0ea4fe11d5
duckodes <bear90789078@gmail.com>
2025-09-30 14:30:40 +0800

    fix params

--- res/module/main.js
+++ res/module/main.js
@@ -96,7 +96,7 @@ const main = (async () => {
         articleView.style.padding = '1rem';
         articleBackButton.style.display = 'none';
         showArticle((await get(ref(database, `technotes/data/${dataKey}`))).val()[urlSearchParams.get('category')][urlSearchParams.get('categoryID')]);
-    } else if (!urlSearchParams.get('categoryID')) {
+    } else if (urlSearchParams.get('category') && !urlSearchParams.get('categoryID')) {
         document.querySelector('.layout').style.display = 'none';
         document.body.appendChild(articleView);
         articleView.style.padding = '1rem';
