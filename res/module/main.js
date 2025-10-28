40e1a0ed366d1f5d3ee99583d775ba1e31255178
duckodes <bear90789078@gmail.com>
2025-10-28 17:49:44 +0800

    update image convert rule

--- res/module/main.js
+++ res/module/main.js
@@ -424,8 +424,11 @@ const main = (async () => {
         });
     }
     function convertToImages(text) {
-        return text.replace(/\[img:([^\[\]]+)\[\[([\s\S]*?)\]\]\]/g, (match, src, alt) => {
-            return `<img src="${src.trim()}" alt="${alt.trim()}" loading="lazy" crossorigin="anonymous" />`;
+        return text.replace(/\[img:([^\[\]]+)(?:\[([^\[\]]*)?(?:\[([^\[\]]*)\])?\])?\]/g, (match, src, alt, width) => {
+            const attrs = [`src="${src.trim()}"`, 'loading="lazy"'];
+            if (alt) attrs.push(`alt="${alt.trim()}"`);
+            if (width) attrs.push(`style="width:${width.trim()}"`);
+            return `<img ${attrs.join(' ')} crossorigin="anonymous" />`;
         });
     }
     function convertToHeadings(text) {
